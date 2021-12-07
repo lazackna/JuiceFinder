fd704707b63e013d6a1087896812cb05340ba78b
David <davidstulemeijer@gmail.com>
2021-12-07 14:59:55 +0100

    Added OCM api key secret

--- Project/app/src/main/java/com/lazackna/juicefinder/MainActivity.java
+++ Project/app/src/main/java/com/lazackna/juicefinder/MainActivity.java
@@ -2,7 +2,10 @@ package com.lazackna.juicefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ApplicationInfo ai = null;
+        try {
+            ai = getPackageManager().getApplicationInfo(getPackageName(), getPackageManager().GET_META_DATA);
+
+        String val = ai.metaData.getString("ocmKey");
+
+        Toast.makeText(this,val,Toast.LENGTH_LONG).show();
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
